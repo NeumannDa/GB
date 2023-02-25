58944c7dc40c62d030aa33e48ff2dfbb55e7f230
TRTSS <felix.trof@gmail.com>
2023-02-25 22:42:10 +0300

    Добавьте файлы проекта.

---
+++ GB/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> words = new List<string>();
+int n = Convert.ToInt32(Console.ReadLine());
+List<string> res = new List<string>();
+
+for (int i = 0; i < n; i++)
+{
+    words.Add(Console.ReadLine());
+}
+
+foreach (string word in words)
+{
+    if (word.Length <= 3) res.Add(word);
+}
+
+Console.WriteLine(String.Join(", ", res));
